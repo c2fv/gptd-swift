734a98cba50a37d429a012a4f1d197d8012b64e8
Chris <cs@mobileboost.io>
2025-02-06 12:04:56 +0100

    typo fix in Package

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "gptd-swift",
-    platforms: [.iOS(.v13)]
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "gptd-swift",
